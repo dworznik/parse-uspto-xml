7f02a7633ac873b31a4e58640a77116184e63b91
Patryk Dwórznik <patryk@dworznik.com>
2023-08-03 21:44:09 +0900

    Fix table/indexes naming and use array types

--- schema.sql
+++ schema.sql
@@ -1,13 +1,19 @@
-CREATE TABLE uspto_patents(
+-- extensions
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE EXTENSION IF NOT EXISTS btree_gin;
+
+-- patent grants
+CREATE TABLE uspto_applications(
         publication_number VARCHAR,
         publication_title VARCHAR,
         publication_date DATE,
         publication_type VARCHAR,
-        authors VARCHAR,
-        sections VARCHAR,
-        section_classes VARCHAR,
-        section_class_subclasses VARCHAR,
-        section_class_subclass_groups VARCHAR,
+        applicants VARCHAR[],
+        inventors VARCHAR[],
+        sections VARCHAR[],
+        section_classes VARCHAR[],
+        section_class_subclasses VARCHAR[],
+        section_class_subclass_groups VARCHAR[],
         abstract TEXT,
         description TEXT,
         claims TEXT,
@@ -22,17 +28,53 @@ CREATE TABLE uspto_patents(
         PRIMARY KEY(publication_number)
 );
 
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
-CREATE EXTENSION IF NOT EXISTS btree_gin;
+CREATE INDEX idx_uspto_applications_publication_date ON uspto_applications (publication_date);
+CREATE INDEX idx_uspto_applications_publication_title ON uspto_applications ((lower(publication_title)));
 
-CREATE INDEX idx_uspto_publication_date ON uspto_patents (publication_date);
-CREATE INDEX idx_uspto_publication_title ON uspto_patents ((lower(publication_title)));
+CREATE INDEX idx_uspto_applications_sections ON uspto_applications USING GIN (sections);
+CREATE INDEX idx_uspto_applications_section_classes ON uspto_applications USING GIN (section_classes);
+CREATE INDEX idx_uspto_applications_section_class_subclasses ON uspto_applications USING GIN (section_class_subclasses);
+CREATE INDEX idx_uspto_applications_section_class_subclass_groups ON uspto_applications USING GIN (section_class_subclass_groups);
 
-CREATE INDEX idx_uspto_sections ON uspto_patents USING GIN (sections);
-CREATE INDEX idx_uspto_section_classes ON uspto_patents USING GIN (section_classes);
-CREATE INDEX idx_uspto_section_class_subclasses ON uspto_patents USING GIN (section_class_subclasses);
-CREATE INDEX idx_uspto_section_class_subclass_groups ON uspto_patents USING GIN (section_class_subclass_groups);
+CREATE INDEX idx_uspto_applications_ts_abstract ON uspto_applications USING gin(ts_abstract);
+CREATE INDEX idx_uspto_applications_ts_description ON uspto_applications USING gin(ts_description);
+CREATE INDEX idx_uspto_applications_ts_claims ON uspto_applications USING gin(ts_claims);
 
-CREATE INDEX idx_uspto_ts_abstract ON uspto_patents USING gin(ts_abstract);
-CREATE INDEX idx_uspto_ts_description ON uspto_patents USING gin(ts_description);
-CREATE INDEX idx_uspto_ts_claims ON uspto_patents USING gin(ts_claims);
+-- patent grants
+CREATE TABLE uspto_grants(
+        publication_number VARCHAR,
+        publication_title VARCHAR,
+        publication_date DATE,
+        publication_type VARCHAR,
+        applicants VARCHAR[],
+        inventors VARCHAR[],
+        sections VARCHAR[],
+        section_classes VARCHAR[],
+        section_class_subclasses VARCHAR[],
+        section_class_subclass_groups VARCHAR[],
+        abstract TEXT,
+        description TEXT,
+        claims TEXT,
+        created_at TIMESTAMP without time zone,
+        updated_at TIMESTAMP without time zone,
+        abstract_words_count INT GENERATED ALWAYS AS (array_length(regexp_split_to_array(abstract, E'\\s+'), 1)) STORED,
+        description_words_count INT GENERATED ALWAYS AS (array_length(regexp_split_to_array(description, E'\\s+'), 1)) STORED,
+        claims_words_count INT GENERATED ALWAYS AS (array_length(regexp_split_to_array(claims, E'\\s+'), 1)) STORED,
+        ts_abstract tsvector GENERATED ALWAYS AS (to_tsvector('english', left(coalesce(abstract, ''), 1024*1024))) STORED,
+        ts_description tsvector GENERATED ALWAYS AS (to_tsvector('english', left(coalesce(description, ''), 1024*1024))) STORED,
+        ts_claims tsvector GENERATED ALWAYS AS (to_tsvector('english', left(coalesce(claims, ''), 1024*1024))) STORED,
+        PRIMARY KEY(publication_number)
+);
+
+
+CREATE INDEX idx_uspto_grants_publication_date ON uspto_grants (publication_date);
+CREATE INDEX idx_uspto_grants_publication_title ON uspto_grants ((lower(publication_title)));
+
+CREATE INDEX idx_uspto_grants_sections ON uspto_grants USING GIN (sections);
+CREATE INDEX idx_uspto_grants_section_classes ON uspto_grants USING GIN (section_classes);
+CREATE INDEX idx_uspto_grants_section_class_subclasses ON uspto_grants USING GIN (section_class_subclasses);
+CREATE INDEX idx_uspto_grants_section_class_subclass_groups ON uspto_grants USING GIN (section_class_subclass_groups);
+
+CREATE INDEX idx_uspto_grants_ts_abstract ON uspto_grants USING gin(ts_abstract);
+CREATE INDEX idx_uspto_grants_ts_description ON uspto_grants USING gin(ts_description);
+CREATE INDEX idx_uspto_grants_ts_claims ON uspto_grants USING gin(ts_claims);
